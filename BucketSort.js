6e16f9ee075caa9804061d4e86bfac23f4d673fe
kennedyBJJ <williankennedy368@gmail.com>
2022-11-21 23:17:25 -0200

    Novos comentários explicando o que cada trecho de código faz

--- BucketSort.js
+++ BucketSort.js
@@ -1,8 +1,12 @@
-var unsortedArray = [3,2,1,-1,0,0,333, -333, 444,442.32,32321.12]
-// bucket sort values into buckets in which values are grouped by their range
-// then for each bucket(bucket is an array) we run an insertion sort on it
+var unsortedArray = [3,2,1,-1,0,0,333, -333, 444,442,323]
+
+// agrupar valores em baldes de acordo com o intervalo definido de cada balde
+// então, para cada balde(balde é um vetor), os ordenamos usando o insertionSort
 function insertionSort(inputArray){
-    if(inputArray.length < 2) return inputArray
+    if(inputArray.length < 2) {
+        return inputArray
+    }
+    
     for(let i=1; i<inputArray.length; i++){
         let temp = inputArray[i]
         for(var j = i-1; j>=0 && inputArray[j]>temp; j--){
@@ -12,29 +16,53 @@ function insertionSort(inputArray){
     }
     return inputArray
 }
-// var unsortedArray = [9,8,7,6,5,4,3,2,1,0]
-
 
 function bucketSort(inputArray){
-    if(inputArray.length < 2) return inputArray
+    //vetor de tamanho 1 já está ordenado
+    if(inputArray.length < 2) {
+        return inputArray
+    }
+
+    //encontrar o valor máximo e o valor mínimo do vetor
     let minValue = inputArray[0]
     let maxValue = inputArray[0]
-    for(let value of inputArray){
-        if(value > maxValue){
-            maxValue = value
-        } else if (value < minValue){
-            minValue = value
-        }
+    for(let value of inputArray){ 
+            maxValue = (value > maxValue)? value : maxValue
+            minValue = (value < minValue)? value : minValue
+    } /*equivalente a:
+        for(let i = 0; i<inputArray.length;i++ ){
+            let value = inputArray[i]
+            maxValue = (value > maxValue)? value : maxValue
+            minValue = (value < minValue)? value : minValue
     }
-    let bucketRange = 5
+*/
+    
+    //definir a quantidade de baldes e a extensão de cada um
+    let bucketRange = 10 
     let bucketArraySize = Math.floor((maxValue - minValue) / bucketRange) + 1 
-    let bucketArray = Array.from({length:bucketArraySize}, ()=>[])
+    let bucketArray = Array.from({length:bucketArraySize}, ()=>[])  /** equivalente a:
+                                                                       let bucketArray = []
+                                                                       for(let i=0 ; i<bucketArraySize ; i++){
+                                                                           bucketArray[i] = []
+                                                                       }
+                                                                    */
+    
+    
+
+    //inserir os valores nos baldes
     for(let value of inputArray){
         bucketArray[Math.floor((value - minValue)/bucketRange)].push(value)
     }
+
+    //ordenar os baldes
     for(let value of bucketArray){
         insertionSort(value)
     }
     return [].concat(...bucketArray)
 }
-console.log(bucketSort(unsortedArray.slice()))
+
+console.log(`vetor não ordenado:\n${unsortedArray}`)
+
+unsortedArray = bucketSort(unsortedArray)
+
+console.log(`vetor ordenado:\n${unsortedArray}`)
